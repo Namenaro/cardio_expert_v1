bd9719b240a81fa8d72707fb7f2eeab3a4271cb7
namenaro <nanenaro@gmail.com>
2025-11-03 17:28:17 +0300

    поля основных сущностей

--- CORE/datasets/LUDB.py
+++ CORE/datasets/LUDB.py
@@ -2,7 +2,7 @@ from CORE.signal_1d import Signal
 
 from importlib import resources
 import json
-from typing import Optional
+from typing import Optional, List
 from types import SimpleNamespace
 
 LUDB_LEADS_NAMES = SimpleNamespace(
@@ -21,26 +21,53 @@ LUDB_LEADS_NAMES = SimpleNamespace(
 )
 
 
-class LUDB_wrapper:
+class LUDB:
     def __init__(self):
         """
             Обертка для работы с датасетом LUDB.
             Загружает данные из JSON-файла и предоставляет методы для доступа к ним.
 
             Attributes:
-                data (dict): Загруженные данные датасета
+                _data (dict): Загруженные данные датасета
 
             Raises:
                 FileNotFoundError: Если файл датасета не найден
             """
         try:
             with resources.files('datasets.data').joinpath('ludb.json').open('r', encoding='utf-8') as f:
-                self.data = json.load(f)
+                self._data = json.load(f)
 
         except FileNotFoundError as e:
             raise FileNotFoundError(
                 f"Файл датасета 'ludb.json' не найден в пакете 'datasets.data'"
             ) from e
 
-    def get_signal(self, patient_id:str, lead_name:LUDB_LEADS_NAMES)->Optional[Signal]:
-        pass
+    def get_1d_signal(self, patient_id:str, lead_name:LUDB_LEADS_NAMES)->Optional[Signal]:
+        """
+                Возвращает одномерный сигнал для указанного пациента и отведения.
+
+                Args:
+                    patient_id: Идентификатор пациента в файле ecg_data_200.json
+                    lead_name: Название отведения из LUDB_LEADS_NAMES
+
+                Returns:
+                    Объект Signal с данными сигнала или None,
+                    если сигнал не найден
+
+                Note:
+                    Исходные данные в LUDB представлены в вольтах, поэтому при создании Signal
+                    выполняется преобразование в милливольты
+        """
+        signal_mV = ... # TODO
+        signal = Signal(signal_mv=signal_mV)
+        return signal
+
+    def get_patients_ids(self)->List[str]:
+        pass # TODO
+
+if __name__ == "__main__":
+    ludb = LUDB()
+    patients_ids = ludb.get_patients_ids()
+
+    signal = ludb.get_1d_signal(patient_id=patients_ids[0], lead_name=LUDB_LEADS_NAMES.i)
+    print(signal)
