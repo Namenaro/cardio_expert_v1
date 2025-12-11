62ab0f2138ad85859ef5a0376b2cbf399078b480
namenaro <nanenaro@gmail.com>
2025-12-11 14:00:13 +0300

    поля основных сущностей

--- DA3/redactors_widgets/pazzles_subwidgets/input_points_widget.py
+++ DA3/redactors_widgets/pazzles_subwidgets/input_points_widget.py
@@ -10,5 +10,6 @@ from CORE.db_dataclasses import *
 class InputPointsWidget(QWidget):
     """Виджет для редактирования входных параметров объекта"""
 
+
     def __init__(self, parent=None):
         super().__init__(parent)
