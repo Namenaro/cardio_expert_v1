9151cdeb6f86f759f5582b90651a681b9d11c85f
namenaro <nanenaro@gmail.com>
2026-01-13 16:29:20 +0300

    реформат

--- CORE/paths.py
+++ CORE/paths.py
@@ -8,3 +8,17 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Основая база данных, заполненная в DA
 DB_NAME = "test_database.db"
 DB_PATH = os.path.join(BASE_DIR, "data", DB_NAME)
+
+# Папка с json-файлами датасетов экземплярв форм, заполняемых в Datagen
+EXEMPLARS_DATASETS_PATH = os.path.join(BASE_DIR, "data", "forms_datasets")
+
+
+def create_directories():
+    """Создаёт директории, если их нет."""
+    directories = [
+        os.path.dirname(DB_PATH),
+
+    ]
+    for dir_path in directories:
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path, exist_ok=True)
