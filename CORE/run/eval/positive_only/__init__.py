a4220e6e6f6726f398de84049081ec7e4b1d226a
Yana <Namenaro@users.noreply.github.com>
2026-03-17 18:02:37 +0300

    написание симулятора - в процессе

--- CORE/run/eval/positive_only/__init__.py
+++ CORE/run/eval/positive_only/__init__.py
@@ -1,5 +1,13 @@
-from CORE.run.eval.positive_only.kde_eval import KDE_Eval
-from CORE.run.eval.positive_only.mahalanobis_dist_eval import MahalanobisEval
-from CORE.run.eval.positive_only.mahalonobis_nonparametric_eval import MahalanobisNonparametricEval
+from CORE.run.eval.positive_only.kde import KDE_Eval
+from CORE.run.eval.positive_only.mahalanobis_dist import MahalanobisEval
+from CORE.run.eval.positive_only.mahalonobis_nonparametric import MahalanobisNonparametricEval
 from CORE.run.eval.positive_only.sum_dists_eval import SumDistsEval
-from CORE.run.eval.positive_only.mann_whitney_eval import MannWhitneyEval
+from CORE.run.eval.positive_only.mann_whitney import MannWhitneyEval
+from CORE.run.eval.positive_only.isolation_forest_percentile import IsolationForestPercentileEvaluator
+from CORE.run.eval.positive_only.LOF_percentile import LOFPercentileEvaluator
+from CORE.run.eval.positive_only.one_class_SVM import OneClassSVMEvaluator
+from CORE.run.eval.positive_only.robust_mahalanobis import EllipticEnvelopeEvaluator
+
+__all__ = ['KDE_Eval', 'MahalanobisEval', 'MahalanobisNonparametricEval',
+           'SumDistsEval', 'MannWhitneyEval', 'IsolationForestPercentileEvaluator',
+           'LOFPercentileEvaluator', 'OneClassSVMEvaluator', 'EllipticEnvelopeEvaluator']
