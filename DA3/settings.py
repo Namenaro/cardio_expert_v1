ef7ecdcef14c8891e6969d2c46c1a9c97565ed7b
Yana <Namenaro@users.noreply.github.com>
2026-03-17 18:10:07 +0300

    написание симулятора - в процессе

--- DA3/settings.py
+++ DA3/settings.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
+from typing import Type
+
+from CORE.run.eval.base_eval import BaseEvaluator
+from CORE.run.eval.positive_only import *
 
 
 @dataclass
 class Settings:
     max_half_padding_from_real_coord_of_first: float = 0.05
+    evaluator_class: Type[BaseEvaluator] = EllipticEnvelopeEvaluator
